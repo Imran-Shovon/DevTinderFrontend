82b56edab28eb3ae0cb2b7dc1eda3318f9de3bd6
Imran-Shovon <shovonbiswas.cse@gmail.com>
2026-06-24 23:30:45 +0600

    Routing Added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer sm:footer-horizontal footer-center bg-base-200 text-base-content p-4 fixed bottom-0 w-full">
+        <aside>
+          <p>
+            Copyright © {new Date().getFullYear()} - All right reserved by
+            SHOVON
+          </p>
+        </aside>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
